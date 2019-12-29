2535ecabfcbd2684ba28c0193ff2e1b2dca1c26e
florian99797 <florian.koehler1999@gmail.com>
2019-12-29 09:25:28 +0100

    Teilseite mit Aussicht auf mögliche Gewinne, die zum spielen motivieren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,8 +36,8 @@ export class AppComponent {
       },
       {
         isActive: false,
-        text: 'Menu Entry 2',
-        href: '/menuEntry2',
+        text: 'Rewards',
+        href: '/rewards',
       },
       {
         isActive: false,
