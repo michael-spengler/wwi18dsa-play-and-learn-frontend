6c6e2eed9241eead12d1cc61129f6970f40faaf2
joshua <joshua.schmieder@arcor.de>
2020-01-30 14:49:26 +0100

    DancePlus_Frontend

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,105 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+/* 1. selecteer elementen met document.querySelector */
+var section = document.querySelector('section.dancer'); // first section element
+var button = document.querySelector('section > button');
+
+var dance = function () {
+  /* 3. toggle een class op een element, dit triggert een CSS Transition (zie CSS: section.dance) */
+	section.classList.toggle('dancing')
+}
+
+/* 2. voeg een event toe aan een element */
+button.addEventListener('click', dance);
+
+/*Discokugel startet hier*/
+var t = 1;
+var radius = 50;
+var squareSize = 6.5;
+var prec = 19.55;
+var fuzzy = 0.001;
+var inc = (Math.PI-fuzzy)/prec;
+var discoBall = document.getElementById("discoBall");
+
+for(var t=fuzzy; t<Math.PI; t+=inc) {
+  var z = radius * Math.cos(t);
+  var currentRadius = Math.abs((radius * Math.cos(0) * Math.sin(t)) - (radius * Math.cos(Math.PI) * Math.sin(t))) / 2.5;
+  var circumference = Math.abs(2 * Math.PI * currentRadius);
+  var squaresThatFit = Math.floor(circumference / squareSize);
+  var angleInc = (Math.PI*2-fuzzy) / squaresThatFit;
+  for(var i=angleInc/2+fuzzy; i<(Math.PI*2); i+=angleInc) {
+    var square = document.createElement("div");
+    var squareTile = document.createElement("div");
+    squareTile.style.width = squareSize + "px";
+    squareTile.style.height = squareSize + "px";
+    squareTile.style.transformOrigin = "0 0 0";
+    squareTile.style.webkitTransformOrigin = "0 0 0";
+    squareTile.style.webkitTransform = "rotate(" + i + "rad) rotateY(" + t + "rad)";
+    squareTile.style.transform = "rotate(" + i + "rad) rotateY(" + t + "rad)";
+    if((t>1.3 && t<1.9) || (t<-1.3 && t>-1.9)) {
+      squareTile.style.backgroundColor = randomColor("bright");
+    } else {
+      squareTile.style.backgroundColor = randomColor("any");
+    }
+    square.appendChild(squareTile);
+    square.className = "square";
+    squareTile.style.webkitAnimation = "reflect 2s linear infinite";
+    squareTile.style.webkitAnimationDelay = String(randomNumber(0,20)/10) + "s";
+    squareTile.style.animation = "reflect 2s linear infinite";
+    squareTile.style.animationDelay = String(randomNumber(0,20)/10) + "s";
+    squareTile.style.backfaceVisibility = "hidden";
+    var x = radius * Math.cos(i) * Math.sin(t);
+    var y = radius * Math.sin(i) * Math.sin(t);
+    square.style.webkitTransform = "translateX(" + Math.ceil(x) + "px) translateY(" + y + "px) translateZ(" + z + "px)";
+    square.style.transform = "translateX(" + x + "px) translateY(" + y + "px) translateZ(" + z + "px)";
+    discoBall.appendChild(square);
+  }
+}
+
+function randomColor(type) {
+  var c;
+  if(type == "bright") {
+    c = randomNumber(130, 255);
+  } else {
+    c = randomNumber(110, 190);
+  }
+  return "rgb(" + c + "," + c + "," + c + ")";
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+/*Discokugel endet hier*/
+
+/*Darkmode beginnt hier*/
+
+
+
+import * as $ from "jquery";
+
+$(function() {
+  $("#bt").click(function() {
+       $(".contentbox").toggleClass("contentbox-change");  $(".spreadbubble").toggleClass("spreadbubble-change");
+$(".spreadbubble-change2").toggleClass("spreadbubble2");
+
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
